633253828f23e94fb4a45601afbde52671dc4c74
SeanBrogan <broganpsean@gmail.com>
2017-09-16 19:09:16 -0500

    Database is mapping to the Java class

--- src/util/HibernateUtil.java
+++ src/util/HibernateUtil.java
@@ -14,27 +14,29 @@ public class HibernateUtil {
 
         public static void main(String[] args) {
 
-                Configuration cf = new Configuration().configure("hibernate.cfg.xml");
-
-                StandardServiceRegistryBuilder srb = new StandardServiceRegistryBuilder();
-                srb.applySettings(cf.getProperties());
-                ServiceRegistry sr = srb.build();
-                SessionFactory sf = cf.buildSessionFactory(sr);
-
-                Session session = sf.openSession();
-                medicalRecord medRec = new medicalRecord();
-                medRec.setRecordId(1);
-                medRec.setPatientId(2);
-                medRec.setDoctorId(3);
-                medRec.setTreatment("nothing");
-                medRec.setDescription("something");
-                medRec.setRecordDate(new Date());
-
-                Transaction tx = session.beginTransaction();
-                session.save(medRec);
-                tx.commit();
-                System.out.println("Object saved successfully.....!!");
-                session.close();
-                sf.close();
+        	Configuration cf = new Configuration().configure("hibernate.cfg.xml");
+        	
+        	cf.addClass(medicalRecord.class);
+        	
+        	StandardServiceRegistryBuilder srb = new StandardServiceRegistryBuilder();
+        	srb.applySettings(cf.getProperties());
+        	ServiceRegistry sr = srb.build();
+        	SessionFactory sf = cf.buildSessionFactory(sr);
+        	
+        	Session session = sf.openSession();
+        	medicalRecord medRec = new medicalRecord();
+        	medRec.setRecordId(1);
+        	medRec.setPatientId(2);
+        	medRec.setDoctorId(3);
+        	medRec.setTreatment("nothing");
+        	medRec.setDescription("something");
+        	medRec.setRecordDate(new Date());
+        	
+        	Transaction tx = session.beginTransaction();
+        	session.save(medRec);
+        	tx.commit();
+        	System.out.println("Object saved successfully.....!!");
+        	session.close();
+        	sf.close();
         }
 }
