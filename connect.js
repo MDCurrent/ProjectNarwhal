10f599f18d72c183d9b97656d17e5144c567b517
SeanBrogan <broganpsean@gmail.com>
2017-09-22 12:47:16 -0500

    node js initial integration

---
+++ connect.js
@@ -0,0 +1,23 @@
+var Connection = require('tedious').Connection;
+var Request = require('tedious').Request;
+var TYPES = require('tedious').TYPES;
+
+// Create connection to database
+var config = {
+  userName: 'test1234', // update me
+  password: '1234', // update me
+  server: 'localhost',
+  options: {
+      database: 'SampleDB'
+  }
+}
+var connection = new Connection(config);
+
+// Attempt to connect and execute queries if connection goes through
+connection.on('connect', function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Connected');
+  }
+});
